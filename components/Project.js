65ced0655fbabe1229c21ef47d4eb66822df3547
BerkinAKKAYA <berkin_akkaya@hotmail.com>
2021-09-19 10:07:31 +0000

    add live links

--- components/Project.js
+++ components/Project.js
@@ -1,6 +1,13 @@
 import styles from "../styles/Project.module.css";
 
-export default function Header({ title, description, index, img }) {
+export default function Header({
+  title,
+  description,
+  index,
+  img,
+  liveLink,
+  githubLink,
+}) {
   return (
     <div className={styles.container + " " + styles["hover" + index]}>
       <img src={img} alt={title} />
@@ -9,8 +16,18 @@ export default function Header({ title, description, index, img }) {
         <h2>{title}</h2>
         <p style={{ marginTop: 15 }}>{description}</p>
         <div className={styles.actions}>
-          <button>Live</button>
-          <button>GitHub</button>
+          <a href={liveLink} target="_blank" rel="noreferrer">
+            <button>Live</button>
+          </a>
+          {githubLink ? (
+            <a href={githubLink} target="_blank" rel="noreferrer">
+              GitHub
+            </a>
+          ) : (
+            <a href="#" className={styles.disabled}>
+              GitHub
+            </a>
+          )}
         </div>
       </div>
     </div>
--- pages/projects.js
+++ pages/projects.js
@@ -15,30 +15,40 @@ export default function Projects() {
           description="Chat Application Backed by Firestore"
           img="/project-thumbnails/svelte.png"
           index="1"
+          liveLink="https://svelte-chat-berkinakkaya.web.app/"
+          githubLink="https://github.com/BerkinAKKAYA/svelte-chat"
         />
         <Project
           title="Vueweeter"
           description="Realtime Social Media App"
           img="/project-thumbnails/vue.png"
           index="2"
+          liveLink="https://berkinakkaya.github.io/vueweeter"
+          githubLink="https://github.com/BerkinAKKAYA/vueweeter"
         />
         <Project
           title="svelte-image-gallery"
           description="Fluid Image Container for Svelte"
           img="/project-thumbnails/npm.svg"
           index="3"
+          liveLink="http://npmjs.com/package/svelte-image-gallery"
+          githubLink="https://github.com/BerkinAKKAYA/svelte-image-gallery"
         />
         <Project
           title="Stay In The Shadows"
           description="Stealth & Action Game"
           img="/project-thumbnails/stay-in-the-shadows.png"
           index="4"
+          liveLink="https://play.google.com/store/apps/details?id=com.BerkinAkkaya.StayintheShadows"
+          githubLink=""
         />
         <Project
           title="Timeline"
           description="Most Minimalistic Calendar Ever"
           img="/project-thumbnails/timeline.png"
           index="5"
+          liveLink="http://timeline-berkin.web.app/"
+          githubLink="https://github.com/BerkinAKKAYA/Timeline"
         />
       </div>
     </>
