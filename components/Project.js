0eff4064000c19defb58823e1070c73c7842a552
BerkinAKKAYA <berkin_akkaya@hotmail.com>
2021-09-19 09:43:43 +0000

    add project content

--- components/Project.js
+++ components/Project.js
@@ -1,30 +1,13 @@
 import styles from "../styles/Project.module.css";
 
-export default function Header({ title, right, index }) {
+export default function Header({ title, description, index, img }) {
   return (
-    <div
-      className={
-        styles.container +
-        " " +
-        (right ? styles.right : styles.left) +
-        " " +
-        styles["hover" + index]
-      }
-    >
-      <img src="/project-thumbnails/jumpy.png" alt={title} />
+    <div className={styles.container + " " + styles["hover" + index]}>
+      <img src={img} alt={title} />
 
       <div className={styles.content}>
         <h2>{title}</h2>
-        <p style={{ marginTop: 15 }}>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam
-          feugiat ex nec odio ornare, at tristique tortor suscipit. Suspendisse
-          potenti. Cras varius, nunc ullamcorper fermentum imperdiet, dolor
-          augue molestie elit, ac pretium mauris nibh nec neque.
-        </p>
-
-        <a href="#" className={styles.details}>
-          Click To See More
-        </a>
+        <p style={{ marginTop: 15 }}>{description}</p>
       </div>
     </div>
   );
--- pages/projects.js
+++ pages/projects.js
@@ -10,11 +10,36 @@ export default function Projects() {
       </Head>
 
       <div className={styles.container}>
-        <Project title="Project 1" index="1" />
-        <Project title="Project 2" index="2" right />
-        <Project title="Project 3" index="3" />
-        <Project title="Project 4" index="4" right />
-        <Project title="Project 5" index="5" />
+        <Project
+          title="Svelte Chat"
+          description="Chat Application Backed by Firestore"
+          img="/project-thumbnails/svelte.png"
+          index="1"
+        />
+        <Project
+          title="Vueweeter"
+          description="Realtime Social Media App"
+          img="/project-thumbnails/vue.png"
+          index="2"
+        />
+        <Project
+          title="svelte-image-gallery"
+          description="Fluid Image Container for Svelte"
+          img="/project-thumbnails/npm.svg"
+          index="3"
+        />
+        <Project
+          title="Stay In The Shadows"
+          description="Stealth & Action Game"
+          img="/project-thumbnails/stay-in-the-shadows.png"
+          index="4"
+        />
+        <Project
+          title="Timeline"
+          description="Most Minimalistic Calendar Ever"
+          img="/project-thumbnails/timeline.png"
+          index="5"
+        />
       </div>
     </>
   );
