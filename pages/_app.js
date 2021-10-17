c462ed33fdcc725f15dda89b249e093db61da3cc
BerkinAKKAYA <berkin_akkaya@hotmail.com>
2021-10-17 16:13:18 +0000

    formatting

--- pages/_app.js
+++ pages/_app.js
@@ -4,40 +4,40 @@ import TransitionLayout from "./TransitionLayout.js";
 import Router from "next/router";
 
 function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link
-          rel="stylesheet"
-          media="screen"
-          href="https://fontlibrary.org//face/fantasque-sans-mono"
-          type="text/css"
-        />
-      </Head>
+	return (
+		<>
+			<Head>
+				<link
+					rel="stylesheet"
+					media="screen"
+					href="https://fontlibrary.org//face/fantasque-sans-mono"
+					type="text/css"
+				/>
+			</Head>
 
-      <TransitionLayout>
-        <Component {...pageProps} />
-      </TransitionLayout>
-    </>
-  );
+			<TransitionLayout>
+				<Component {...pageProps} />
+			</TransitionLayout>
+		</>
+	);
 }
 
 export default App;
 
 const routeChange = () => {
-  // Temporary fix to avoid flash of unstyled content
-  // during route transitions. Keep an eye on this
-  // issue and remove this code when resolved:
-  // https://github.com/vercel/next.js/issues/17464
+	// Temporary fix to avoid flash of unstyled content
+	// during route transitions. Keep an eye on this
+	// issue and remove this code when resolved:
+	// https://github.com/vercel/next.js/issues/17464
 
-  const tempFix = () => {
-    const allStyleElems = document.querySelectorAll('style[media="x"]');
-    console.log(allStyleElems);
-    allStyleElems.forEach((elem) => {
-      elem.removeAttribute("media");
-    });
-  };
-  tempFix();
+	const tempFix = () => {
+		const allStyleElems = document.querySelectorAll('style[media="x"]');
+		console.log(allStyleElems);
+		allStyleElems.forEach((elem) => {
+			elem.removeAttribute("media");
+		});
+	};
+	tempFix();
 };
 
 Router.events.on("routeChangeComplete", routeChange);
